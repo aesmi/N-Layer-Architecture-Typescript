f77bd8efa62d14099475c97b75d1b06233d4291b
Donny Roufs <iamchets@yahoo.com>
2021-03-08 21:31:51 +0100

    Refacto core tests to work with async methods

--- src/Core/User/User.service.spec.ts
+++ src/Core/User/User.service.spec.ts
@@ -1,15 +1,16 @@
 import 'reflect-metadata'
 
 import { Container, injectable } from 'inversify'
-import { User as UserModule } from '../../Config/DI/Modules/User.module'
+import { UserModule } from '../../Config/DI/Modules/User.module'
 import { Types } from '../../Config/DI/Types'
 import { IUserRepository } from './Ports/IUserRepository.port'
 import { IUserService } from './Ports/IUserService.port'
-import { User } from './User.entity'
+import { IUserEntity } from './IUserEntity'
+import { CoreValidationException } from '../Lib/Exceptions/CoreValidation.exception'
 
 @injectable()
 class RepoMock implements IUserRepository {
-  save(payload: User): User {
+  async save(payload: IUserEntity) {
     return payload
   }
 }
@@ -28,16 +29,19 @@ describe('UserService', () => {
     userService = container.get<IUserService>(Types.UserService)
   })
 
-  it('Should give me a created user back', () => {
-    const createdUser = userService.createUser({ name: 'Donny' })
+  it('Should give me a created user back', async (done) => {
+    const createdUser = await userService.createUser({ name: 'Donny' })
 
     expect(createdUser).toBeDefined()
     expect(createdUser.name).toEqual('Donny')
+    done()
   })
 
-  it('Should throw an exception when the provided name is shorter than 4 characters', () => {
-    expect(() => {
-      userService.createUser({ name: 'abc' })
-    }).toThrow()
+  it('Should throw an exception when the provided name is shorter than 4 characters', async (done) => {
+    expect(async () => {
+      await userService.createUser({ name: 'abc' })
+    }).rejects.toBeInstanceOf(CoreValidationException)
+
+    done()
   })
 })
